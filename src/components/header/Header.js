4f595498f11cd5fbbe654474b7ba7b034803cdd6
Vlad Bolofinov <vlad.bolofinov@gmail.com>
2023-04-11 14:12:54 +0300

    done header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,30 @@
 import './Header.scss'
 import logo from '../../img/icons/logo.svg';
+import userIcon from '../../img/icons/userIcon.svg';
+import heartIcon from '../../img/icons/heartIcon.svg';
+import lockIcon from '../../img/icons/lockIcon.svg';
+
+
 const Header = () => {
     return (
         <div className='header'>
-            <img className='logo' src={logo}/>
+            <img className='logo' src={logo} alt='logo'/>
             <div className='menu'>
                 <p>Женщины</p>
                 <p>Мужчины</p>
                 <p>Поиск</p>
             </div>
             <input/>
+            <div className='language'>
+                <p className="RU">RU</p>
+                <p className="UA">UA</p>
+                <p className="EN">EN</p>
+            </div>
+            <div className='userIcons'>
+                <img src={userIcon} alt="userIcon"/>
+                <img src={heartIcon} alt="heartIcon"/>
+                <img src={lockIcon} alt="lockIcon"/>
+            </div>
         </div>
     )
 }
