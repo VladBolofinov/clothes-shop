fd7607531c85c27a7223ab3314c179a294475258
Vlad Bolofinov <vlad.bolofinov@gmail.com>
2023-04-21 14:48:03 +0300

    add collection-block

--- src/components/collection/Collection.js
+++ src/components/collection/Collection.js
@@ -1,12 +1,21 @@
 import './Collection.scss';
-import girl1 from '../../img/items/img-new-collection.png';
 
 const Collection = () => {
     return (
         <div className='collection-wrapper'>
-            <img src={girl1}/>
+            <div className="collection-girl1">
+                <h1 className='collection-header'>New<br/>Arrival</h1>
+                <p className='collection-descr'>
+                    Style and comfort in every detail: discover the new collection of women's clothing from our online store.
+                </p>
+                <a className='collection-link' href="#">Смотреть коллекцию</a>
+            </div>
             <div className='square'>
-
+                <div className='collection-girl2'>
+                    <p className='collection-descr'>
+                        Embodiment of style: update your wardrobe with the new collection from our store.
+                    </p>
+                </div>
 
             </div>
         </div>
